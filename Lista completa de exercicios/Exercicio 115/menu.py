badd3f3a4ff9422b4e3c620301d3906abf98ca27
Lucas Noffs Motta <lnoffs@hotmail.com>
2024-05-24 19:36:27 -0700

    Update menu.py

--- Lista completa de exercicios/Exercicio 115/menu.py
+++ Lista completa de exercicios/Exercicio 115/menu.py
@@ -1,26 +1,32 @@
 from cores import cores
 import dados
+from time import sleep
 
 cor = cores()
 
 def opcoes():
 
-    file = open('pessoas.txt','a')
+    names_file = open('nomes.txt','a')
+
+
 
     while True:
         dados.mostra_titulo('MENU PRINCIPAL')
+        sleep(0.5)
         print(f'{cor["amarelo"]} 1) - {cor["azul"]} Ver pessoas cadastradas')
         print(f'{cor["amarelo"]} 2) - {cor["azul"]} Cadastrar nova pessoa')
         print(f'{cor["amarelo"]} 3) - {cor["azul"]} Sair do sistema{cor["limpa"]}')
         escolha = dados.valida_num(f'{cor["amarelo"]}Sua opcao: {cor["limpa"]}')
 
         if escolha in '1':
-            dados.mostra_titulo("Pessoas cadastradas")
-            lista = dados.le_pessoas()
-            dados.le_lista(lista)
+            sleep(0.5)
+            dados.mostra_titulo("PESSOAS CADASTRADAS")
+            lista_nomes = dados.le_pessoas()
+            dados.le_lista(lista_nomes)
 
 
         elif escolha in '2':
+            sleep(0.5)
             dados.cadastra_pessoa()
 
         elif escolha in '3':
@@ -32,7 +38,4 @@ def opcoes():
 
 
 
-
-
-opcoes()
 
